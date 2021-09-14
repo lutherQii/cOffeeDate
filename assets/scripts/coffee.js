13eecaa6ea503b1d845de5840678488ea4c6b532
wesleyjayfrisby <wesfrisby@gmail.com>
2021-09-14 21:42:40 +0200

    Added coffe page and js for page

---
+++ assets/scripts/coffee.js
@@ -0,0 +1,9 @@
+const mainOption = document.getElementById('mainOptionsDIV');
+
+function loginClick() {
+    mainOption.style.display = "none";
+}
+
+function registerClick() {
+    mainOption.style.display = "none";
+}

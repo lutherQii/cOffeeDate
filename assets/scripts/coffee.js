c9646f55bdde24ad5f2bbcd4d836f103fa3a6e00
wesleyjayfrisby <wesfrisby@gmail.com>
2021-09-18 12:38:21 +0200

    Coffee changes

--- assets/scripts/coffee.js
+++ assets/scripts/coffee.js
@@ -1,9 +1,42 @@
-const mainOption = document.getElementById('mainOptionsDIV');
+const mainOptionDiv = document.getElementById('mainOptionsDIV');
+const loginDiv = document.getElementById('loginDIV');
+const registerStep1Div = document.getElementById('registerStep1Div');
+const registerOTPDiv = document.getElementById('registerOTPDiv');
+const txtUserName = document.getElementById('TxtUsername');
+const txtPassword = document.getElementById('TxtPassword');
+const txtRegisterUsername = document.getElementById('TxtRegisterUserName');
+const txtRegisterEmail = document.getElementById('TxtRegisterEmail');
+const txtRegisterCell = document.getElementById('TxtRegisterCell');
+const txtRegisterPassword = document.getElementById('TxtRegisterPassword');
+const txtOTP = document.getElementById('TxtOTP');
 
 function loginClick() {
-    mainOption.style.display = "none";
+    mainOptionDiv.style.display = "none";
+    loginDiv.style.display = "block";
 }
 
 function registerClick() {
-    mainOption.style.display = "none";
+    mainOptionDiv.style.display = "none";
+    registerStep1Div.style.display = "block";
+}
+function loginNextClick() {
+    alert('Username: ' + txtUserName.value + ' Pasword: ' + txtPassword.value);
+}
+function registerNextClick() {
+    alert('Username: ' + txtRegisterUsername.value + ' Email: ' + txtRegisterEmail.value
+        + ' Cell: ' + txtRegisterCell.value + ' Password: ' + txtRegisterPassword.value);
+        registerStep1Div.style.display = "none";
+        registerOTPDiv.style.display = "block";
+}
+function resendOTPClick() {
+    alert('Resend OTP');
+}
+function otpNextClick() {
+    alert('OTP: ' + txtOTP.value);
+}
+function refisterFBClick() {
+    alert('Register FB');
+}
+function refisterAppleClick() {
+    alert('Register Apple');
 }
